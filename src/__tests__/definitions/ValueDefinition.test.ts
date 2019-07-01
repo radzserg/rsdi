f902f32075d028ef814a3e1ce9d5bd6f6661abe8
Sergey Radzishevskyi <radzserg@gmail.com>
2019-07-01 11:23:44 +0300

    Adds ValueDefinition
    Keeps raw value inside definition.

---
+++ src/__tests__/definitions/ValueDefinition.test.ts
@@ -0,0 +1,9 @@
+import ValueDefinition from "definitions/ValueDefinition";
+
+describe("ValueDefinition", () => {
+    test("it resolves value", () => {
+        const definition = new ValueDefinition("ENV", "production");
+        expect(definition.name()).toEqual("ENV");
+        expect(definition.resolve()).toEqual("production");
+    });
+});
---
+++ src/definitions/ValueDefinition.ts
@@ -0,0 +1,17 @@
+import BaseDefinition from "definitions/BaseDefinition";
+
+export default class ValueDefinition extends BaseDefinition {
+    private readonly value: any;
+
+    constructor(
+        name: string,
+        value: any
+    ) {
+        super(name);
+        this.value = value;
+    }
+
+    resolve = <T>(): T => {
+        return this.value as T;
+    }
+}

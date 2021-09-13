44452f36ed8d6234f373d64a10fafd169db62d35
Sergey Radzishevskyi <radzserg@gmail.com>
2021-09-13 17:47:32 +0300

    updates ValueDefinition typings

--- src/definitions/ValueDefinition.ts
+++ src/definitions/ValueDefinition.ts
@@ -3,15 +3,15 @@ import BaseDefinition from "./BaseDefinition";
 /**
  * Raw value definition
  */
-export default class ValueDefinition extends BaseDefinition {
+export default class ValueDefinition<T extends any> extends BaseDefinition<T> {
     private readonly value: any;
 
-    constructor(value: any) {
+    constructor(value: T) {
         super();
         this.value = value;
     }
 
-    resolve = <T>(): T => {
-        return this.value as T;
+    resolve = <Y extends T>(): Y => {
+        return this.value as Y;
     };
 }
--- src/definitions/__tests__/ValueDefinition.test.ts
+++ src/definitions/__tests__/ValueDefinition.test.ts
@@ -6,3 +6,23 @@ describe("ValueDefinition", () => {
         expect(definition.resolve()).toEqual("production");
     });
 });
+
+describe("it respects TS types", function() {
+    test("infer types", () => {
+        const definition = new ValueDefinition<string>("production");
+        let s: string = definition.resolve();
+        expect(s).toEqual("production");
+    });
+
+    test("it uses constructor type", () => {
+        const definition = new ValueDefinition<string>("production");
+        let s: string = definition.resolve();
+        expect(s).toEqual("production");
+    });
+
+    test("it respects resolve type parameter", () => {
+        const definition = new ValueDefinition(123);
+        let n: number = definition.resolve<number>();
+        expect(n).toEqual(123);
+    });
+});

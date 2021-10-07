5772607acb532c39753df9e4a9b43b365f25bad8
Sergey Radzishevskyi <radzserg@gmail.com>
2021-10-07 20:54:55 +0300

    adds more tests for function resolver

--- src/resolvers/__tests__/FunctionResolver.test.ts
+++ src/resolvers/__tests__/FunctionResolver.test.ts
@@ -3,6 +3,15 @@ import { diUse } from "../../resolversShorthands";
 import DIContainer from "../../DIContainer";
 
 describe(FunctionResolver.name, () => {
+    test("it accepts raw values as parameters", () => {
+        function factoryA(s: string) {
+            return s;
+        }
+
+        const resolver = new FunctionResolver(factoryA, "abc");
+        expect(resolver.resolve(new DIContainer())).toEqual("abc");
+    });
+
     test("it accepts references as parameters", () => {
         class Logger {
             public info(message: string) {
@@ -17,15 +26,25 @@ describe(FunctionResolver.name, () => {
             };
         }
 
-        const definition = new FunctionResolver(
-            createProcessTask,
-            diUse(Logger)
-        );
+        const resolver = new FunctionResolver(createProcessTask, diUse(Logger));
         const container = new DIContainer();
         container.add({
             Logger: new Logger(),
         });
-        const processTask = definition.resolve(container);
+        const processTask = resolver.resolve(container);
         expect(processTask(new Task())).toEqual("Done");
+    });
+
+    test("it accepts raw and references as parameters", () => {
+        function factoryA(s: string, n: number) {
+            return s + n;
+        }
+
+        const container = new DIContainer();
+        container.add({
+            n: 3,
+        });
+        const resolver = new FunctionResolver(factoryA, "20", diUse("n"));
+        expect(resolver.resolve(container)).toEqual("203");
     });
 });
